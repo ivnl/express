f75c34cc8cd503cfb30a991e72701aaabda3ec6b
Ivan <ivansaur@gmail.com>
2016-06-14 15:17:32 -0400

    added dynamic view by changing color of home page on every load

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 var express = require('express');
 var app = express();
 
-
-
 var exphbs  = require('express-handlebars');
+var colors = ["Red", "Blue", "Green", "Purple", "Pink", "Yellow", "Orange"];
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
-$(document).ready(function() {
-    console.log("index.js loaded");
-});
-
 app.get('/', function (req, res) {
   res.render('home');
 });
-
 app.get('/home', function (req, res) {
-  res.render('home');
+	var randomColor = colors[Math.floor(Math.random()*colors.length)];
+	res.render('home', { inspirational: randomColor });
 });
 app.get('/about', function (req, res) {
   res.render('about');
@@ -25,6 +21,7 @@ app.get('/about', function (req, res) {
 app.get('/inspiration', function (req, res) {
   res.render('inspiration');
 });
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
